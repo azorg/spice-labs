0d90c37a6859f0231591a1da7cb51bbf867845d6
Alex Zorg <azorg@mail.ru>
2015-10-16 12:53:09 +0300

    add lab-4

--- lab-4/_ngspice.sh
+++ lab-4/_ngspice.sh
@@ -3,8 +3,14 @@
 # номер лабораторной работы
 i=$(basename `pwd` | sed 's/lab-//')
 
-# расширение SPICE модели
-ext="ckt"
+INP="lab-${i}.sch"
+OUT="lab-${i}.ckt"
 
-ngspice "lab-${i}.${ext}"
+BACKEND="spice-sdb"
+#BACKEND="spice" # bad idea
+
+# ключ `sort_mode` нужен для правильной работы gnucap
+gnetlist -g "$BACKEND" -O sort_mode -o "$OUT" "$INP"
+
+ngspice "$OUT"
 
