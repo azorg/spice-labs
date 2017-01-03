90f5e1c1973148782f38a6a90a1c49822b62200b
Alex Zorg <azorg@mail.ru>
2017-01-03 22:58:35 +0300

    strange update...

---
+++ lab-5/_ngspice.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# номер лабораторной работы
+i=$(basename `pwd` | sed 's/lab-//')
+
+INP="lab-${i}.sch"
+OUT="lab-${i}.ckt"
+
+BACKEND="spice-sdb"
+#BACKEND="spice" # bad idea
+
+# ключ `sort_mode` нужен для правильной работы gnucap
+gnetlist -g "$BACKEND" -O sort_mode -o "$OUT" "$INP"
+
+ngspice "$OUT"
+
